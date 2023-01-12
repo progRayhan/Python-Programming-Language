7abdea25f68372792f2d2623533b9547581b3def
Rayhan <prog.rayhan1@gmail.com>
2023-01-12 15:04:54 +0600

    style should be constent

---
+++ pep 8/Tim Guideline/01-general_pep8_guidelines.py/01-style_constent.py
@@ -0,0 +1,10 @@
+# if use single quotes once, then always use single quotes
+a = "I am in single quotes"
+
+
+# if use double quotes, then always use double quotes
+b = "I am in double quotes"
+
+
+# if use space around your operators then always use it
+c = 5 + 6
