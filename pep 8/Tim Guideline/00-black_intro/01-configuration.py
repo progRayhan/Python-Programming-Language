7fb3ea08bd7884f43293594e80be0572f34c4887
Rayhan <prog.rayhan1@gmail.com>
2023-01-12 14:51:00 +0600

    pep configuration added

---
+++ pep 8/Tim Guideline/00-black_intro/01-configuration.py
@@ -0,0 +1,11 @@
+"""
+vs code > settings > search : python format provider > select : black
+"""
+
+
+"""
+vs code > settings > search : format > check : format on save
+"""
+
+
+# apply it when code save
